8567d650685066c2e1a06722c397292f3d510eec
Danylo Pomirchyi <pomircijdanilo@gmail.com>
2025-04-17 10:49:41 +0300

    Очистив main для чистої бази

--- src/main/java/com/bookstore/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore.repository;
-
-import com.bookstore.model.Book;
-import java.util.List;
-
-public interface BookRepository {
-    Book save(Book book);
-    List<Book> findAll();
-}
--- src/main/java/com/bookstore/service/BookService.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore.service;
-
-import com.bookstore.model.Book;
-import java.util.List;
-
-public interface BookService {
-    Book save(Book book);
-    List<Book> findAll();
-}
