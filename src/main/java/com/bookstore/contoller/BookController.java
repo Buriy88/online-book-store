749838aa4bfd1d2bd7be374a363bb4322726835a
Danylo Pomirchyi <pomircijdanilo@gmail.com>
2025-05-13 14:59:24 +0300

    hw3-soft-delete-liquibase

--- src/main/java/com/bookstore/contoller/BookController.java
+++ src/main/java/com/bookstore/contoller/BookController.java
@@ -5,14 +5,9 @@ import com.bookstore.dto.CreateBookRequestDto;
 import com.bookstore.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -43,6 +38,7 @@ public class BookController {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
     }
--- src/main/java/com/bookstore/model/Book.java
+++ src/main/java/com/bookstore/model/Book.java
@@ -1,15 +1,24 @@
 package com.bookstore.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
+import org.hibernate.annotations.SQLRestriction;
 import java.math.BigDecimal;
 
 @Entity
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 @SQLDelete(sql = "UPDATE books SET deleted = true WHERE id=?")
-@Where(clause = "deleted=false")
+@SQLRestriction("deleted=false")
 @Table(name = "books")
 public class Book {
     @Id
@@ -28,8 +37,7 @@ public class Book {
     @Column(nullable = false)
     private BigDecimal price;
 
-    @Lob
-    @Column
+    @Column(length = 255)
     private String description;
 
     private String coverImage;
