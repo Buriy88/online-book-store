1cd71120cbdb5db864685be99e5a6cc4e295f411
Danylo Pomirchyi <pomircijdanilo@gmail.com>
2025-06-04 15:50:13 +0300

    hw7

--- src/main/java/com/bookstore/model/User.java
+++ src/main/java/com/bookstore/model/User.java
@@ -17,6 +17,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
