bb24a81c483380c61bf3a11bbe4ece6a9b3f1393
Buriy88 <pomircijdanilo@gmail.com>
2026-02-01 23:26:15 +0200

    method find in entityManager

--- src/main/java/com/bookstore/contoller/BookController.java
+++ src/main/java/com/bookstore/contoller/BookController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
@@ -28,6 +27,7 @@ public class BookController {
     public Page<BookDto> getAll(Pageable pageable) {
         return bookService.findAll(pageable);
     }
+
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.getBookById(id);
